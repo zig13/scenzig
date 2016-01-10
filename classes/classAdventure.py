b9a4b1671c74f5d6e406e271c63b0c5d84651882
zig13 <zig13@users.noreply.github.com>
2016-01-10 21:09:39 +0000

    Adventure class now discerns it's relative path on initialisation. Wrote the validate mechanic which checks for the presence of the Adventure's data files and returns True or False to the main script.

--- classes/classAdventure.py
+++ classes/classAdventure.py
@@ -1,5 +1,11 @@
 class Adventure :
+	datafiles = ['abilities', 'actions',  'actiongrps', 'attributes', 'classifications', 'currencies', 'encounters', 'items', 'main', 'scenes', 'vitals']
 	def __init__(self, foldername) :
-		pass
+		from os import curdir, sep
+		self.directory = curdir+sep+"Adventures"+sep+foldername+sep
 	def validate(self) :
-		pass
+		from os import access, R_OK
+		for datafile in self.datafiles :
+			if not access(self.directory+datafile+".scnz", R_OK) : return False
+		print "Pass" #Temporary
+		return True
