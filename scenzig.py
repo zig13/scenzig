0fe7f42c084466a72e936e228d89181c2d7c7f9c
zig13 <zig13@users.noreply.github.com>
2016-01-10 15:23:16 +0000

    Initial commit of main script

---
+++ scenzig.py
@@ -0,0 +1,11 @@
+﻿#-------------------------------------------------------------------------------
+# Name:        scenzig Main Script
+# Purpose:     An engine for text-based adventure games and interactive prose using a scene-based system.
+#
+# Author:      Thomas Sturges-Allard
+#
+# Created:     09/01/2016
+# Copyright:   (c) Thomas Sturges-Allard 2016
+# Licence:      Licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.
+#			http://creativecommons.org/licenses/by-nc-sa/3.0/
+#-------------------------------------------------------------------------------
