9d06055ecfcb18f917999c39c3541b70bf919e43
zig13 <zig13@users.noreply.github.com>
2016-01-17 23:41:55 +0000

    Implemented loading of data files and a test read. Data files will be accessed from the main script as e.g. a.f[scenes]

--- classes/classAdventure.py
+++ classes/classAdventure.py
@@ -1,7 +1,7 @@
 class Adventure :
 	datafiles = ['abilities', 'actions',  'actiongrps', 'attributes', 'classifications', 'currencies', 'encounters', 'items', 'main', 'scenes', 'vitals']
 	def __init__(self, foldername) :
-		from os import curdir, sep
+		from os import curdir, sep #sep and curdir produce the correct characters for the operating system in use
 		self.directory = curdir+sep+"Adventures"+sep+foldername+sep
 	def validate(self) :
 		from os import access, R_OK
@@ -9,4 +9,13 @@ class Adventure :
 			if not access(self.directory+datafile+".scnz", R_OK) : return False
 		return True
 	def load(self) :
-		print "Loading..." #Temporary
+		from configobj import ConfigObj
+		self.f = dict((datafile, ConfigObj(self.directory+datafile+".scnz", unrepr=True)) for datafile in self.datafiles) #Opens each data file up for reading referenced as an entry in the dictionary 'f'
+		for datafile in self.datafiles :
+			print "checking "+datafile #Temporary
+			try:
+				self.f[datafile]['0'] #Checks that a section exists in each data file entitled 0
+			except:
+				print datafile+" failed" #Temporary
+				return False
+		return True
--- scenzig.py
+++ scenzig.py
@@ -9,23 +9,32 @@
 # Licence:      Licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.
 #			http://creativecommons.org/licenses/by-nc-sa/3.0/
 #-------------------------------------------------------------------------------
-from os import curdir, sep, listdir
+from os import curdir, sep, listdir #sep and curdir produce the correct characters for the operating system in use
 try :
 	adventures = listdir(curdir+sep+"Adventures"+sep)
 except OSError :
 	raw_input("Adventures folder missing or incorrectly named.")
 	exit(0)
 from classes.classAdventure import Adventure
-adv = dict((foldername, Adventure(foldername)) for foldername in adventures)
-from functions import valremove, choicelist
-for adventure in adventures :
+adv = dict((foldername, Adventure(foldername)) for foldername in adventures) #Creates an instance of classAdventure for each folder found in the Adventures folder as a dictionary entry of adv
+from functions import valremove, choicelist, Clr
+for adventure in adventures : #Runs the validate function of each instance of classAdventure. If they return False (fail) then they are removed from the Adventures list
 	if adv[adventure].validate() == False : adventures = valremove(adventures, adventure)
-if len(adventures) == 1 :
-	a = adv[adventures[0]]
-elif len(adventures) < 1 :
+if len(adventures) < 1 :
 	raw_input("No valid adventures installed.")
 	exit(0)
+elif len(adventures) == 1 :
+	a = adv[adventures[0]] #If only one ~valid Adventure exists, automatically loads it without asking
+	if a.load() == False :
+		raw_input("No valid adventures installed.")
+		exit(0)	
 else :
-	a = adv[choicelist(adventures, "Please enter a number corresponding to the adventure you wish to load:\n")[1]]
-	print a.directory #Temporary
-	a.load()
+	temptext = "Please enter a number corresponding to the adventure you wish to load:\n"
+	while True :
+		a = adv[choicelist(adventures, temptext)[1]]
+		if a.load() == False :
+			temptext = "The selected adventure failed to load. One or more data files lacked key values.\nPlease select a different Adventure:"
+			Clr()
+		else :
+			break
+#From here 'a' refers to the classAdventure instance of the active Adventure. All Adventure data will be accessed through it.
