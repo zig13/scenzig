49a28e56f0c137f9911b11f85872de72bfbea83b
zig13 <zig13@users.noreply.github.com>
2016-02-14 09:28:02 +0000

    Added support for 4 new operators and in the process fixed some problems with the code. ^ returns the larger of the two operands. _ retuns the smaller of the two operands. ~ returns the average (mean) of the two operands. % returns operandA percent of operandB. Previously operands were not being turned into ints before being operated on. Now they are individually passed to PrsArg which will int( them if nessassary.

--- argparser.py
+++ argparser.py
@@ -40,7 +40,7 @@ def PrsArg(arg) :
 	if isinstance(arg, list) :
 		return [PrsArg(each) for each in arg]	
 	try :
-		opcounts = {'+':arg.count('+'), '-':arg.count('-'), 'x':arg.count('x')}
+		opcounts = {'+':arg.count('+'), '-':arg.count('-'), 'x':arg.count('x'), '^':arg.count('^'), '_':arg.count('_'), '~':arg.count('~'), '%':arg.count('%')}
 		try :
 			return int(arg)
 		except ValueError :
@@ -55,22 +55,25 @@ def PrsArg(arg) :
 			except NameError: #This will occur is the input is something like bert or b20
 				print "The only letters I accept are d (dice roll), v (vital lookup), a (attribute lookup) and c (currency lookup).\nThe letter should be immediately followed by the number of dice sides for d or ID for v, a and c."
 				return 0
+			except SyntaxError:
+				print "Only intergers can follow d (dice roll), v (vital lookup), a (attribute lookup) and c (currency lookup)"
+				return 0
 		elif optotal == 1 :
 			oprtr = {vr: ky for ky, vr in opcounts.iteritems()}[1] #Inverts the opcounts dictionary so the count is the key and the operator is the value. We can then quickly find the operator with a count of 1
 			tup = arg.partition(oprtr) #Creates a tuple that will look something like 'd8', '+', '4'
-			operandA = tup[0]
-			operandB = tup[2]
+			operandA = PrsArg(tup[0])
+			operandB = PrsArg(tup[2])
 		elif optotal == 2 :
 			if arg[-1] is not ")" :
 				tup = arg.partition(")") #Creates a tuple that will look something like '(d8+4', ')', 'x2'
-				operandA = tup[0][1:] #From the example above cuts out just d8+4
+				operandA = PrsArg(tup[0][1:]) #From the example above cuts out just d8+4
 				oprtr = tup[2][0] #From the example above cuts out just x
-				operandB = tup[2][1:] #From the example above cuts out just 2
+				operandB = PrsArg(tup[2][1:]) #From the example above cuts out just 2
 			elif arg[0] is not "(" :
 				tup = arg.partition("(") #Creates a tuple that will look something like '6+', '(', 'd6x2)'
-				operandA = tup[0][:-1] #From the example above cuts out just 6
+				operandA = PrsArg(tup[0][:-1]) #From the example above cuts out just 6
 				oprtr = tup[0][-1] #From the example above cuts out just +
-				operandB = tup[2][:-1] #From the example above cuts out just d6x2
+				operandB = PrsArg(tup[2][:-1]) #From the example above cuts out just d6x2
 			else : #This should only happen if someone has put in something like (d8+4)x(2)
 				print "Please don't use brackets unnecessarily"
 				return 0
@@ -80,6 +83,10 @@ def PrsArg(arg) :
 		if oprtr is '+': return PrsArg(operandA)+PrsArg(operandB)
 		elif oprtr is '-': return PrsArg(operandA)-PrsArg(operandB)
 		elif oprtr is 'x': return PrsArg(operandA)*PrsArg(operandB)
+		elif oprtr is '^': return max(operandA,operandB) #Returns the greater of two operands
+		elif oprtr is '_': return min(operandA,operandB)
+		elif oprtr is '~': return (operandA+operandB)/2
+		elif oprtr is '%': return (operandA*operandB)/100 #Returns operandA % of operandB
 	except AttributeError : #If argument is already an integer
 		return arg
 		
