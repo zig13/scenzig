1130cb77c2439b79bef1dc6b9157844c71baa394
zig13 <zig13@users.noreply.github.com>
2016-02-28 12:02:55 +0000

    Evaluators, leave effects and enter effects are now optional. Also a number of case corrections to make the script work with scenes and encounters.

--- statecheck.py
+++ statecheck.py
@@ -19,17 +19,19 @@ def AutoState(datafile) :
 		lst = char['Attributes'].keys()
 		currstate = lambda: char['Attributes'][thing][0]
 	elif datafile is 'scenes' :
-		lst = [char['scenes']['current']]
-		currstate = char['scenes']['states'][char['scenes']['current']]
+		lst = [str(char['Scenes']['Current'])]
+		currstate = lambda: char['Scenes']['States'][str(char['Scenes']['Current'])]
 	elif datafile is "encounters" :
-		lst = [char['encounters'][char['scenes']['current']][0]]
-		currstate = char['encounters'][char['scenes']['current']][1]
+		lst = [str(char['Scenes']['Encounters'][str(char['Scenes']['Current'])][0])]
+		currstate = lambda: char['Scenes']['Encounters'][str(char['Scenes']['Current'])][1]
 #	elif datafile is "items" :
 #		lst = char['items']
 #		currstate =	
 	
 	for thing in lst :
-		evaluators = [argparser.PrsArg(each) for each in adv.f[datafile][thing]['evaluators']]
+		try :
+			evaluators = [argparser.PrsArg(each) for each in adv.f[datafile][thing]['evaluators']]
+		except KeyError : break #If there are no evaluators, then we can stop here
 		for state in adv.f[datafile][str(thing)].keys() :
 			this = True
 			try :
@@ -40,15 +42,19 @@ def AutoState(datafile) :
 			except TypeError : this = False
 			if this is True : break
 		if this is True :
-			if currstate() != int(state) :			
-				for effect in adv.f[datafile][str(thing)][str(currstate())]['leaveeffects'].keys() :
-					arguments = argparser.PrsArg(adv.f[datafile][str(thing)][str(currstate())]['leaveeffects'][effect])
-					eval(effect+"(arguments)")
+			if currstate() != int(state) :
+				try :
+					for effect in adv.f[datafile][str(thing)][str(currstate())]['leaveeffects'].keys() :
+						arguments = argparser.PrsArg(adv.f[datafile][str(thing)][str(currstate())]['leaveeffects'][effect])
+						eval(effect+"(arguments)")
+				except KeyError : pass #leave effects are optional
 				if datafile is 'vitals' : char['Vitals'][thing][0] = int(state)
 				elif datafile is 'attributes' : char['Attributes'][thing][0] = int(state)
-				elif datafile is 'scenes' : char['scenes']['states'][char['scenes']['current']] = int(state)
-				elif datafile is "encounters" : char['scenes']['encounters'][char['scenes']['current']][1] = int(state)
+				elif datafile is 'scenes' : char['Scenes']['States'][str(char['Scenes']['Current'])] = int(state)
+				elif datafile is "encounters" : char['Scenes']['Encounters'][str(char['Scenes']['Current'])][1] = int(state)
 #				elif datafile is "items" :
-				for effect in adv.f[datafile][str(thing)][state]['entereffects'].keys() :
-					arguments = argparser.PrsArg(adv.f[datafile][str(thing)][state]['entereffects'][effect])
-					eval(effect+"(arguments)")
+				try :
+					for effect in adv.f[datafile][str(thing)][state]['entereffects'].keys() :
+						arguments = argparser.PrsArg(adv.f[datafile][str(thing)][state]['entereffects'][effect])
+						eval(effect+"(arguments)")
+				except KeyError : pass #enter effects are optional
