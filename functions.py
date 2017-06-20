6637e3c1aa84c3d210d2f5e4dd31dde22a2da9ee
zig13 <zig13@hotmail.co.uk>
2017-06-20 19:08:45 +0100

    nonemptyprint function updated for compatability with the changes to scenzig.py, PrintItems and PrintActions. The newline is dealt with by scenzig.py.

--- functions.py
+++ functions.py
@@ -74,6 +74,7 @@ def get_valid_filename(filename):
 	
 def nonemptyprint(thing):
 	try :
-		print thing['text']+"\n"
+		print thing['text']
+		return True
 	except (TypeError, KeyError):
-		return
+		return False
