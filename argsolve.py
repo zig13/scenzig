046ea1100792612742730427ca12c7ec57d3a042
zig13 <zig13@users.noreply.github.com>
2018-02-17 22:12:48 +0000

    Alterations to Solve to support functions with multiple inputs and multiple-letter names. The changes look more drastic than they are as I removed an unnecessary try/except and in the process reduced the indent of a lot of code. d, a, c, i, t, s, n & l will continue to function as normal but I want the ability to check for a specific state of a specific thing which the single letter functions cannot support.

--- argsolve.py
+++ argsolve.py
@@ -98,58 +98,68 @@ def l(id) : #Returns the id of the label assigned to the given class. Retunns 0 
 		return 0
 
 def Solve(arg) :
+	try :
+		return int(arg)
+	except (ValueError, TypeError) :
+		pass #Argument is not an int yet so process it some more
 	if isinstance(arg, list) :
 		return [Solve(each) for each in arg]
-	try :
-		opcounts = {'+':arg.count('+'), '-':arg.count('-'), 'x':arg.count('x'), '^':arg.count('^'), '_':arg.count('_'), '~':arg.count('~'), '%':arg.count('%')}
-		try :
-			return int(arg)
-		except ValueError :
-			pass #Argument is not an int yet so process it some more
-		optotal = sum(opcounts.values())
-		if optotal == 0 : #If the argument is not yet a number but has no operators. At this point the argument is expected to be something like d8 or v4.
+	opcounts = {'+':arg.count('+'), '-':arg.count('-'), 'x':arg.count('x'), '^':arg.count('^'), '_':arg.count('_'), '~':arg.count('~'), '%':arg.count('%')}
+	optotal = sum(opcounts.values())
+	if optotal == 0 : #If the argument is not yet a number but has no operators. At this point the argument is expected to be something like d8 or SceneHasState,1,1.
+		if arg[:1].isupper() : #If the first character is an uppercase letter
+			csv = arg.split(",") #Splits the string up based on comma placement
+			funct = csv[0]
 			try :
-				return eval(arg[0]+"("+arg[1:]+")") #Calls d, a, v, i or c with the rest of the argument as sides or id
-			except TypeError: #This will occur is the input is something like 8/2. The code attempts to call 8 as a function which fails because it is an integer
-				print "Sorry. The only operators I recognise at the moment are + (add), - (minus) and x (multiply)"
+				arguments = csv[1:]
+			except IndexError :
+				print "The function needs to be followed by it's arguments followed by commas e.g SceneHasStatus,3,6"
 				return 0
-			except NameError: #This will occur is the input is something like bert or b20
-				print "The only letters I accept are d (dice roll), v (vital lookup), a (attribute lookup), i (character has item), c (currency lookup) s0 (current scene) and s# (gives state of given scene).\nThe letter should be immediately followed by the number of dice sides for d or ID for v, a and c."
+			try :
+				return eval(funct+"("+str(arguments)+")")
+			except NameError :
+				print '"'+funct+'"'+"is not valid"
 				return 0
-			except SyntaxError:
-				print "Only integers can follow d (dice roll), v (vital lookup), a (attribute lookup), i (character has item), c (currency lookup) and s (gives state of given scene)."
-				return 0
-		elif optotal == 1 :
-			oprtr = {vr: ky for ky, vr in opcounts.iteritems()}[1] #Inverts the opcounts dictionary so the count is the key and the operator is the value. We can then quickly find the operator with a count of 1
-			tup = arg.partition(oprtr) #Creates a tuple that will look something like 'd8', '+', '4'
-			operandA = Solve(tup[0])
-			operandB = Solve(tup[2])
-		elif optotal == 2 :
-			if arg[-1] is not ")" :
-				tup = arg.partition(")") #Creates a tuple that will look something like '(d8+4', ')', 'x2'
-				operandA = Solve(tup[0][1:]) #From the example above cuts out just d8+4
-				oprtr = tup[2][0] #From the example above cuts out just x
-				operandB = Solve(tup[2][1:]) #From the example above cuts out just 2
-			elif arg[0] is not "(" :
-				tup = arg.partition("(") #Creates a tuple that will look something like '6+', '(', 'd6x2)'
-				operandA = Solve(tup[0][:-1]) #From the example above cuts out just 6
-				oprtr = tup[0][-1] #From the example above cuts out just +
-				operandB = Solve(tup[2][:-1]) #From the example above cuts out just d6x2
-			else : #This should only happen if someone has put in something like (d8+4)x(2)
-				print "Please don't use brackets unnecessarily"
-				return 0
-		else : #This will occur if the input is something like (d8+4)x(2+d4) or ((d8+4)x2)-d10
-			print "Sorry I can't deal with nested brackets at the moment :("
+		try :
+			return eval(arg[0]+"("+arg[1:]+")") #Calls d, a, v, i or c with the rest of the argument as sides or id
+		except TypeError: #This will occur is the input is something like 8/2. The code attempts to call 8 as a function which fails because it is an integer
+			print "Sorry. The only operators I recognise at the moment are + (add), - (minus) and x (multiply)"
 			return 0
-		if oprtr is '+': return Solve(operandA)+Solve(operandB)
-		elif oprtr is '-': return Solve(operandA)-Solve(operandB)
-		elif oprtr is 'x': return Solve(operandA)*Solve(operandB)
-		elif oprtr is '^': return max(operandA,operandB) #Returns the greater of two operands
-		elif oprtr is '_': return min(operandA,operandB)
-		elif oprtr is '~': return (operandA+operandB)/2
-		elif oprtr is '%': return (operandA*operandB)/100 #Returns operandA % of operandB
-	except AttributeError : #If argument is already an integer
-		return arg
+		except NameError: #This will occur is the input is something like bert or b20
+			print "The only letters I accept are d (dice roll), v (vital lookup), a (attribute lookup), i (character has item), c (currency lookup) s0 (current scene) and s# (gives state of given scene).\nThe letter should be immediately followed by the number of dice sides for d or ID for v, a and c."
+			return 0
+		except SyntaxError:
+			print "Only integers can follow d (dice roll), v (vital lookup), a (attribute lookup), i (character has item), c (currency lookup) and s (gives state of given scene)."
+			return 0
+	elif optotal == 1 :
+		oprtr = {vr: ky for ky, vr in opcounts.iteritems()}[1] #Inverts the opcounts dictionary so the count is the key and the operator is the value. We can then quickly find the operator with a count of 1
+		tup = arg.partition(oprtr) #Creates a tuple that will look something like 'd8', '+', '4'
+		operandA = Solve(tup[0])
+		operandB = Solve(tup[2])
+	elif optotal == 2 :
+		if arg[-1] is not ")" :
+			tup = arg.partition(")") #Creates a tuple that will look something like '(d8+4', ')', 'x2'
+			operandA = Solve(tup[0][1:]) #From the example above cuts out just d8+4
+			oprtr = tup[2][0] #From the example above cuts out just x
+			operandB = Solve(tup[2][1:]) #From the example above cuts out just 2
+		elif arg[0] is not "(" :
+			tup = arg.partition("(") #Creates a tuple that will look something like '6+', '(', 'd6x2)'
+			operandA = Solve(tup[0][:-1]) #From the example above cuts out just 6
+			oprtr = tup[0][-1] #From the example above cuts out just +
+			operandB = Solve(tup[2][:-1]) #From the example above cuts out just d6x2
+		else : #This should only happen if someone has put in something like (d8+4)x(2)
+			print "Please don't use brackets unnecessarily"
+			return 0
+	else : #This will occur if the input is something like (d8+4)x(2+d4) or ((d8+4)x2)-d10
+		print "Sorry I can't deal with nested brackets at the moment :("
+		return 0
+	if oprtr is '+': return Solve(operandA)+Solve(operandB)
+	elif oprtr is '-': return Solve(operandA)-Solve(operandB)
+	elif oprtr is 'x': return Solve(operandA)*Solve(operandB)
+	elif oprtr is '^': return max(operandA,operandB) #Returns the greater of two operands
+	elif oprtr is '_': return min(operandA,operandB)
+	elif oprtr is '~': return (operandA+operandB)/2
+	elif oprtr is '%': return (operandA*operandB)/100 #Returns operandA % of operandB
 
 if __name__ == "__main__":
 	print "I can roll dice for you!"
